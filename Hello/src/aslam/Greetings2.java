cbba490700dc76ec5ac6c052bcbf6d2f6b21da59
Mohid Aslam <aslammohid@gmail.com>
2015-09-17 13:35:50 -0400

    Adding HelloWorld, Greetings and Greetings2

---
+++ Hello/src/aslam/Greetings2.java
@@ -0,0 +1,18 @@
+package aslam;
+
+import java.util.Scanner;
+
+public class Greetings2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter your first name: ");
+		String firstName = scan.nextLine();
+		System.out.print("Enter your last name: ");
+		String lastName = scan.nextLine();
+		System.out.println("Hello " + firstName + " " + lastName); 
+		System.out.println("Welcome to Java!");
+
+	}
+
+}
---
+++ Hello/src/aslam/HelloWorld.java
@@ -0,0 +1,9 @@
+package aslam;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");	
+	}
+
+}

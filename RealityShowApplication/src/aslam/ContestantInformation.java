6dbd81f321b626fe92d6a0a3bc40d1a21df8bf69
Mohid Aslam <aslammohid@gmail.com>
2015-09-21 13:02:40 -0400

    Reality Show Program

---
+++ RealityShowApplication/src/aslam/ContestantInformation.java
@@ -0,0 +1,69 @@
+package aslam;
+
+public class ContestantInformation {
+
+	private String firstName, lastName, streetName, streetNumber, city, province, postalCode;
+	private int birthDate, phoneNumber;
+	
+	public ContestantInformation(){
+	
+	}
+	public ContestantInformation(String a, String b, String c, String d, String e, String f, String g, int h, int i){
+		
+	}
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	public void getFirstName(){
+		
+	}
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	public void getLastName(){
+		
+	}
+	public void setStreetName(String streetName){
+		this.streetName = streetName;
+	}
+	public void getStreetName(){
+		
+	}
+	public void setStreetNumber(String streetNumber){
+		this.streetNumber = streetNumber;
+	}
+	public void getStreetNumber(){
+		
+	}
+	public void setCity(String city){
+		this.city = city;
+	}
+	public void getCity(){
+		
+	}
+	public void setProvince(String province){
+		this.province = province;
+	}
+	public void getProvince(){
+		
+	}
+	public void setPostalCode(String postalCode){
+		this.postalCode = postalCode;
+	}
+	public void getPostalCode(){
+		
+	}
+	public void setBirthDate(int birthDate){
+		this.birthDate = birthDate;
+	}
+	public void getBirthDate(){
+		
+	}
+	public void setPhoneNumber(int phoneNumber){
+		this.phoneNumber = phoneNumber;
+	}
+	public void getPhoneNumber(){
+		
+	}
+}
+
